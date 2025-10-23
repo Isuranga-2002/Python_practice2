67086f3b42a196bc67e8118de53e11bc00c222d9
Isuranga-2002 <166277109+Isuranga-2002@users.noreply.github.com>
2025-10-23 11:05:00 +0530

    Add initial implementation of set operations for fruits

---
+++ Data Structures/sets.py
@@ -0,0 +1,13 @@
+# Write code below 💖
+my_fruits = {'Mango','Orange','Papaya','Guvava','Pineapple'}
+
+friend_fruits = {'Woodapple','Mango','Pineapple','Grape','Banana'}
+
+union_result = my_fruits.union(friend_fruits)
+intersection_result = my_fruits.intersection(friend_fruits)
+difference_result = my_fruits.difference(friend_fruits)
+
+print(union_result)
+print(intersection_result)
+print(difference_result)
+
