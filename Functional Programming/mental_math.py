53e8a434df35d0d3c39c464184fe607df2f28d7e
Isuranga-2002 <166277109+Isuranga-2002@users.noreply.github.com>
2025-11-12 09:05:10 +0530

    Fix variable name and output display for even numbers in mental_math.py

--- Functional Programming/mental_math.py
+++ Functional Programming/mental_math.py
@@ -1,7 +1,7 @@
 
 numbers = [57, 10, 82, 36, 89, 46, 13, 23, 92, 48]
 
-even_number = [num for num in numbers if num % 2 == 0]
+even_numbers = [num for num in numbers if num % 2 == 0]
 
 print(f"Original Numbers:{numbers}")
-print(f"Even Numbers:{even_number}")
+print(f"Even Numbers:{even_numbers}")
