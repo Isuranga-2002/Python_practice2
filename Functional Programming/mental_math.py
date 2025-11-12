274190b6f2fc6b708b6d56d0516b515de91748a3
Isuranga-2002 <166277109+Isuranga-2002@users.noreply.github.com>
2025-11-12 09:03:18 +0530

    Fix output display in mental_math.py to show original and even numbers clearly

--- Functional Programming/mental_math.py
+++ Functional Programming/mental_math.py
@@ -3,4 +3,5 @@ numbers = [57, 10, 82, 36, 89, 46, 13, 23, 92, 48]
 
 even_number = [num for num in numbers if num % 2 == 0]
 
-print(even_number)
+print(f"Original Numbers:{numbers}")
+print(f"Even Numbers:{even_number}")
