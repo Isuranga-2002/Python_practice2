318098ce54523b9d5380fd21ccf47347922e0cba
Isuranga-2002 <166277109+Isuranga-2002@users.noreply.github.com>
2025-11-12 08:44:20 +0530

    Refactor grammy_awards.py for improved readability and structure; format playlist and ensure converted durations are reused correctly

--- Functional Programming/grammy_awards.py
+++ Functional Programming/grammy_awards.py
@@ -1,6 +1,14 @@
-# List of songs with their durations (in minutes)
-playlist = [('What Was I Made For?', 3.42), ('Just Like That', 5.05), ('Song 3', 6.55), ('Leave The Door Open', 4.02), ('I Can\'t Breath', 4.47), ('Bad Guy', 3.14)]
+from functools import reduce
 
+# List of songs with their durations (in minutes)
+playlist = [
+    ('What Was I Made For?', 3.42),
+    ('Just Like That', 5.05),
+    ('Song 3', 6.55),
+    ('Leave The Door Open', 4.02),
+    ("I Can't Breath", 4.47),
+    ('Bad Guy', 3.14)
+]
 
 def longer_than_5(song):
     return song[1] > 5
@@ -11,6 +19,13 @@ print(list(longer_songs))
 def convert_to_seconds(song):
     return song[1] * 60 
 
-converted_to_seconds = map(convert_to_seconds,playlist)     
-print(list(converted_to_seconds))  
+# Convert map to list immediately to reuse
+converted_to_seconds = list(map(convert_to_seconds, playlist))
+print(converted_to_seconds)  
 
+def add(a, b):
+    return a + b
+
+Summation_in_seconds = reduce(add, converted_to_seconds)
+
+print(f"Complete time: {Summation_in_seconds}")
